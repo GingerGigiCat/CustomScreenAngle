cc1d6aaa9771ae51d672c3fcc09f7debe0473646
gigi <cat@gigicat.co.uk>
2024-10-12 13:22:44 +0100

    Made it a bit better

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import math
 import os
+import subprocess
 
-def calc_x_shift(angle, v_res):
+def calc_x_shift(angle, v_res): # Calculate the amount of pixels to shift the display horizontrally by, using fancy trigonometry.
     print(f"Angle: {angle}")
     print(f"V_res: {v_res}")
     print(f"sin{angle} = {round(math.sin(math.radians(angle)), 8)}") # math.radians is needed because python maths is weird and to prevent catastrophic failure.
@@ -25,18 +26,20 @@ def calc_transform_matrix(angle, v_res):
     print(f"Transform: {matrix}")
     return matrix
 
-def form_xrandr_command(matrix, display_name="HDMI-1"):
+def form_xrandr_command(matrix: list, max_size: int, display_name: str ="HDMI-1"):
     matrix_but_string = ""
     for value in matrix: # Convert the array to a string that xrandr will like
         matrix_but_string += f"{value},"
     matrix_but_string = matrix_but_string[:-1] # Remove the last character because it is a comma
 
-    command = f"xrandr --output {display_name} --transform {matrix_but_string}"
+    command = f"xrandr --output {display_name} --transform {matrix_but_string} --fb {max_size}x{max_size}"
     print(command)
     return command
 def take_inputs():
     v_res = int(input("Enter V-Resolution: "))
-    angle = int(input("Enter Angle: "))
-    form_xrandr_command(calc_transform_matrix(angle, v_res))
+    h_res = int(input("Enter H-Resolution: "))
+    while True:
+        angle = int(input("Enter Angle: "))
+        os.system(form_xrandr_command(calc_transform_matrix(angle, v_res), v_res+h_res))
 
 take_inputs()
